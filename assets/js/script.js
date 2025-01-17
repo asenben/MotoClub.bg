2ce568658e0f35e69e71d430e75c7b7f248ed767
asenben <asen.benev@gmail.com>
2025-01-18 01:13:16 +0200

    Добавена функционалност за превключване на бургер меню и икони - Обединена логика за превключване на менюто и иконите (open/close) - Показване на иконата за затваряне при активно меню - Скриване на иконата за отваряне при отворено меню - Обновени CSS стилове за поддръжка на адаптивност

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 document.querySelector('.taggle-main').addEventListener('click', () => {
     const menu = document.querySelector('.main-menu ul');
+    const openIcon = document.querySelector('.open-icon');
+    const closeIcon = document.querySelector('.close-icon');
+
     menu.classList.toggle('active');
+
+    if (menu.classList.contains('active')) {
+        openIcon.style.display = 'none';
+        closeIcon.style.display = 'inline-block';
+    } else {
+        openIcon.style.display = 'inline-block';
+        closeIcon.style.display = 'none';
+    }
 });
