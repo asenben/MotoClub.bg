9a211b8e2e5bf876e3753e16fc43693f7ced8737
asenben <asen.benev@gmail.com>
2025-01-19 12:10:44 +0200

    Промених стила на header.css и  script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,34 @@
 function mainMenu() {
   const menu = document.querySelector(".main-menu ul");
   const menuIcon = document.querySelector(".toggle-menu");
+  const menuItems = document.querySelectorAll(".main-menu ul li");
   const openIcon = document.querySelector(".open-icon");
   const closeIcon = document.querySelector(".close-icon");
-  const menuItems = document.querySelectorAll(".main-menu ul li");
 
   menuIcon.addEventListener("click", () => {
-    if (menu.classList.contains("active")) {
-      menuItems.forEach((item, index) => {
-        setTimeout(() => {
-          item.classList.remove("visible");
-        }, index * 100);
-      });
+      if (menu.classList.contains("active")) {
+          menuItems.forEach((item, index) => {
+              setTimeout(() => {
+                  item.classList.remove("visible");
+              }, index * 100); 
+          });
 
-      setTimeout(() => {
-        menu.style.maxHeight = "0";
-        setTimeout(() => menu.classList.remove("active"), 300);
-        openIcon.style.display = "inline-block";
-        closeIcon.style.display = "none";
-      }, menuItems.length * 100);
-    } else {
-      menu.classList.add("active");
-      menu.style.maxHeight = menu.scrollHeight + "px";
+          setTimeout(() => {
+              menu.classList.remove("active");
+              openIcon.style.display = "inline-block";
+              closeIcon.style.display = "none";
+          }, menuItems.length * 100);
+      } else {
+          menu.classList.add("active");
+          openIcon.style.display = "none";
+          closeIcon.style.display = "inline-block";
 
-      menuItems.forEach((item, index) => {
-        setTimeout(() => {
-          item.classList.add("visible");
-        }, index * 100);
-      });
-
-      openIcon.style.display = "none";
-      closeIcon.style.display = "inline-block";
-    }
-    document.body.style.overflow = menu.classList.contains("active") ? "hidden" : "auto";
+          menuItems.forEach((item, index) => {
+              setTimeout(() => {
+                  item.classList.add("visible");
+              }, index * 100); 
+          });
+      }
   });
 }
 mainMenu();
-
-function highlightActiveLink() {
-  const menuLinks = document.querySelectorAll('.main-menu ul li a');
-  const currentPath = window.location.pathname.replace(/\/$/, "").toLowerCase();
-
-  menuLinks.forEach(link => {
-      let linkPath = link.getAttribute('href').replace(/\/$/, "").toLowerCase();
-
-      if (!linkPath.startsWith("/")) {
-          linkPath = "/" + linkPath;
-      }
-
-      if (linkPath === currentPath) {
-          link.classList.add('active');
-      }
-  });
-}
-highlightActiveLink();
